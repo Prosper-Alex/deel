0a9dc864ab52d9e4776709974dc7ad54ff78f61e
Prosper Alex <167307872+Prosper-Alex01010@users.noreply.github.com>
2025-03-31 12:38:01 +0100

    feat: update tool box layout and integrate Swiper.js for improved functionality

---
+++ script.js
@@ -0,0 +1,16 @@
+// const swiper = new Swiper(".swiper", {
+//   // Optional parameters
+//   direction: "vertical",
+//   loop: true,
+
+//   // pagination bullets
+//   pagination: {
+//     el: ".swiper-pagination",
+//   },
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
